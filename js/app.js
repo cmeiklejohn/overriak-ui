b5bb8d15320b6f1a1426cda03476ab01a7e00ea4
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2012-01-14 04:31:43 -0500

    Add some code to initialize all of the models recursively.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,53 @@
-var App = Em.Application.create();
+var App = Ember.Application.create();
 
-// Create the data store.
+// Store
 //
-App.store = DS.Store.create();
+App.store = DS.Store.create({
+  adapter: DS.RESTAdapter
+});
+
+// Node Sample
+//
+App.Sample = DS.Model.extend({ 
+  cpuAvg5: DS.attr('string'),
+  createdAt: DS.attr('date'),
+  didLoad: function() { 
+    console.log('Sample loaded; id=' + this.get('id'));
+  }
+});
+
+// Node
+//
+App.Node = DS.Model.extend({
+  ipAddress: DS.attr('string'),
+  samples: DS.hasMany(App.Sample),
+  didLoad: function() { 
+    console.log('Node loaded; id=' + this.get('id'));
+
+    // TODO: Hack to force enumeration and eager loading.
+    // TODO: Find a way to either obtain this from a mixin or inheritance.
+    this.get('samples').map(function(item, index, self) { 
+      return item
+    });
+  }
+});
+
+// Cluster
+//
+App.Cluster = DS.Model.extend({
+  name: DS.attr('string'),
+  nodes: DS.hasMany(App.Node),
+  didLoad: function() { 
+    console.log('Cluster loaded; id=' + this.get('id'));
+
+    // TODO: Hack to force enumeration and eager loading.
+    // TODO: Find a way to either obtain this from a mixin or inheritance.
+    this.get('nodes').map(function(item, index, self) { 
+      return item
+    });
+  }
+});
+
+App.ready = (function() { 
+  App.store.find(App.Cluster, '1');
+});
